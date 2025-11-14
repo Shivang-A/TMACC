05aca21a649cce98e5c569b6c0146be07428c5d8
Shivang <shivangagarwal96@gmail.com>
2025-11-14 18:20:48 -0500

    Refactor temperature unit explanation formatting
    Removed unnecessary line breaks in temperature unit explanation.

--- app.py
+++ app.py
@@ -427,21 +427,16 @@ st.markdown(
     """
 <div class="unit-explanation">
 <strong>📏 Understanding Temperature Units</strong><br><br>
-
 <strong>Temperature scale:</strong> Degrees Celsius (°C)<br>
 <strong>Value scale:</strong> We use two units depending on magnitude:<br><br>
-
 &#8226; <strong>m°C (milli-degrees Celsius)</strong> = 10<sup>-3</sup> °C = 0.001 °C<br>
 &nbsp;&nbsp;&nbsp;Used for larger impacts (≥ 1 m°C)<br><br>
-
 &#8226; <strong>μ°C (micro-degrees Celsius)</strong> = 10<sup>-6</sup> °C = 0.000001 °C<br>
 &nbsp;&nbsp;&nbsp;Used for smaller impacts (&lt; 1 m°C)<br>
 &nbsp;&nbsp;&nbsp;1000 μ°C = 1 m°C<br><br>
-
 <strong>Examples:</strong><br>
 &#8226; 450.25 μ°C = 0.45025 m°C = 0.00045025 °C<br>
 &#8226; 1200.50 μ°C = 1.20050 m°C = 0.00120050 °C<br><br>
-
 <strong>Context:</strong> While these values may seem small, they represent significant impacts when considering:<br>
 &#8226; This is from <em>one state</em> in India, not the entire country or world<br>
 &#8226; Global warming has increased by ~1.5°C since pre-industrial times<br>
